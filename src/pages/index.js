67e4052284414271bf513b2357bb255406060d8f
Bryan Oyloe <boyloe@gmail.com>
2021-01-03 10:03:22 -0700

    Add clipart for Covid and doctor chat

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,8 @@ import HeroImage from '../svg/HeroImage';
 import SvgCharts from '../svg/SvgCharts';
 import ChatScreenShot from '/Users/bryanoyloe/Igedla/gatsby-starter-lander/src/assets/IgedlaChatScreenShot.png'
 import IgedlaLogo from '/Users/bryanoyloe/Igedla/gatsby-starter-lander/src/assets/cropped-logo-igedla.png'
+import CovidTransparent from '/Users/bryanoyloe/Igedla/gatsby-starter-lander/src/assets/coronavirus_transparent.png'
+import DoctorVisits from '/Users/bryanoyloe/Igedla/gatsby-starter-lander/src/assets/doctor-visits.png'
 
 
 export default () => (
@@ -34,7 +36,7 @@ export default () => (
     </section>
     <section id="features" className="py-20 lg:pb-40 lg:pt-48">
       <div className="container mx-auto text-center">
-        <h2 className="text-3xl lg:text-5xl font-semibold">Main Features</h2>
+        <h2 className="text-3xl lg:text-5xl font-semibold">Let's Get Started</h2>
         <div className="flex flex-col sm:flex-row sm:-mx-3 mt-12">
           <div className="flex-1 px-3">
             <Card className="mb-8">
@@ -67,31 +69,31 @@ export default () => (
       id="services"
       primarySlot={
         <div className="lg:pr-32 xl:pr-48">
-          <h3 className="text-3xl font-semibold leading-tight">Market Analysis</h3>
+          <h3 className="text-3xl font-semibold leading-tight">Assess your risk for Covid-19</h3>
           <p className="mt-8 text-xl font-light leading-relaxed">
-            Our team of enthusiastic marketers will analyse and evaluate how your company stacks
-            against the closest competitors
+            Are you at a higher risk of COVID complications, experiencing symptoms, or recently exposed. 
+            Igedla can help you determine if you're positive and the best treatment for you. 
           </p>
         </div>
       }
-      secondarySlot={<SvgCharts />}
+      secondarySlot={<img src={CovidTransparent} alt='covid-logo' />}
     />
     <SplitSection
       reverseOrder
       primarySlot={
         <div className="lg:pl-32 xl:pl-48">
           <h3 className="text-3xl font-semibold leading-tight">
-            Design And Plan Your Business Growth Steps
+            Check your symptoms
           </h3>
           <p className="mt-8 text-xl font-light leading-relaxed">
-            Once the market analysis process is completed our staff will search for opportunities
-            that are in reach
+            Whether you have abnormal cholesterol, depression, or a simple cold, you can easily
+            learn more about your symptoms and get in contact with an online doctor if needed. 
           </p>
         </div>
       }
-      secondarySlot={<SvgCharts />}
+      secondarySlot={<img src={DoctorVisits} alt='chatscreen-clipart' />}
     />
-    <SplitSection
+    {/* <SplitSection
       primarySlot={
         <div className="lg:pr-32 xl:pr-48">
           <h3 className="text-3xl font-semibold leading-tight">
@@ -104,10 +106,10 @@ export default () => (
         </div>
       }
       secondarySlot={<SvgCharts />}
-    />
-    <section id="stats" className="py-20 lg:pt-32">
+    /> */}
+    {/* <section id="stats" className="py-20 lg:pt-32">
       <div className="container mx-auto text-center">
-        <LabelText className="text-gray-600">Our customers get results</LabelText>
+        <LabelText className="text-gray-600">Igedla gets results</LabelText>
         <div className="flex flex-col sm:flex-row mt-8 lg:px-24">
           <div className="w-full sm:w-1/3">
             <StatsBox primaryText="+100%" secondaryText="Stats Information" />
@@ -120,7 +122,7 @@ export default () => (
           </div>
         </div>
       </div>
-    </section>
+    </section> */}
     <section id="testimonials" className="py-20 lg:py-40">
       <div className="container mx-auto">
         <LabelText className="mb-8 text-gray-600 text-center">See why our customers love us</LabelText>
@@ -133,10 +135,10 @@ export default () => (
         </div>
       </div>
     </section>
-    <section className="container mx-auto my-20 py-24 bg-gray-200 rounded-lg text-center">
-      <h3 className="text-5xl font-semibold">Ready to grow your business?</h3>
+    <section className="container mx-auto my-20 py-24 bg-blue-300 rounded-lg text-center">
+      <h3 className="text-5xl font-semibold">How are you feeling today?</h3>
       <p className="mt-8 text-xl font-light">
-        Quis lectus nulla at volutpat diam ut. Enim lobortis scelerisque fermentum dui faucibus in.
+        Download Igedla and try it out for yourself
       </p>
       <p className="mt-8">
         <Button size="xl">Get Started Now</Button>
