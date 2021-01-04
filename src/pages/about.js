ab9de53f06164c09cbd38157a9829be6ffae2305
Bryan Oyloe <boyloe@gmail.com>
2021-01-04 11:18:03 -0700

    Disable ally linting for media files

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from 'react';
 import IgedlaVideo from '../assets/Igedla-final.mp4';
 
-
-export const about = () => {
-    return (
-        <div>
-            <h1>What is Igedla?</h1>
-            <video controls>
-                <source src={IgedlaVideo} type="video/mp4" />
-            </video>
-        </div>
-    )
-}
+const About = () => {
+  return (
+    <div>
+      <h1>What is Igedla?</h1>
+      <video controls>
+        <source src={IgedlaVideo} type="video/mp4" />
+      </video>
+    </div>
+  );
+};
+export default About;
