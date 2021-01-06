8aa50f124f799de5cd6de3859a9d2730cb746308
Bryan Oyloe <boyloe@gmail.com>
2021-01-05 18:41:16 -0700

    About update (#10)
    * changed banner image
    
    * new update to about page
    
    Co-authored-by: William Robersone <william.robersone@gmail.com>

---
+++ src/data/about-data.js
@@ -0,0 +1,27 @@
+
+export default [
+  {
+    title: 'COVID-19 Assessment',
+    content:
+      'Other symptom checkers I have tried were too slow and asked all these random questions, Igedla was fast and to the point.',
+    customerName: 'Eric K.',
+    customerTitle: 'Igedla User',
+    customerImage: 'https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+  },
+  {
+    title: 'Symptoms Checker',
+    content:
+      'Iggy has great personality and guided me smoothly through the symptom checker with ease. ',
+    customerName: 'Sara C. ',
+    customerTitle: 'Igedla User',
+    customerImage: `https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60`
+  },
+  {
+    title: 'Diagnosis',
+    content:
+      'I had another headache and just wanted some quick answers to my questions. Iggy was intuitive and wonderful to use. ',
+    customerName: 'Chantal W.',
+    customerTitle: 'Igedla User',
+    customerImage: 'https://images.unsplash.com/photo-1499952127939-9bbf5af6c51c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1355&q=80'
+  }
+];
