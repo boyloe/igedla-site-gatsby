7d76eb610a895d99429d78394c0e803614efd951
Bryan Oyloe <boyloe@gmail.com>
2021-01-07 14:35:11 -0700

    Add all favicon sizes and update home page text

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
+import Head from './Head'
 import Footer from './Footer';
 import Header from './Header';
-import favicon from '../../assets/logo-square.png';
 
 const Layout = ({ children }) => {
   return (
     <>
-      <Helmet>
-        <title>Igedla - Check symptoms, get fast results</title>
-        <link rel="icon" href={favicon} />
-        <meta
-          name="google-site-verification"
-          content="1V8-wr0sqaFly9Uu158yzQnd5EX8Vb11YdYsRhNruyQ"
-        />
-      </Helmet>
+      <Head />
       <Header />
       <main className="text-gray-900">{children}</main>
       <Footer />
