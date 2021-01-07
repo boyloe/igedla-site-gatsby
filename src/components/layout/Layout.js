86fd6f9f534297223f56c6b2fb9eab802b35dd0e
Bryan Oyloe <boyloe@gmail.com>
2021-01-07 08:44:40 -0700

    Add title and google verification meta tags

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Helmet from 'react-helmet';
+import { Helmet } from 'react-helmet';
 import Footer from './Footer';
 import Header from './Header';
 import favicon from '../../assets/logo-square.png';
@@ -7,8 +7,13 @@ import favicon from '../../assets/logo-square.png';
 const Layout = ({ children }) => {
   return (
     <>
-      <Helmet title="Igedla - Check symptoms, get fast results!" defer={false}>
+      <Helmet>
+        <title>Igedla - Check symptoms, get fast results</title>
         <link rel="icon" href={favicon} />
+        <meta
+          name="google-site-verification"
+          content="1V8-wr0sqaFly9Uu158yzQnd5EX8Vb11YdYsRhNruyQ"
+        />
       </Helmet>
       <Header />
       <main className="text-gray-900">{children}</main>
